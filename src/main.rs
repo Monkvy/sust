0daea2012be70a231ed01bba3220d68debe7d1f4
Monkvy <moluecke@gmx.de>
2022-10-18 20:09:26 +0200

    sdl2 test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 
-fn events(e: &winit::event::WindowEvent) -> bool {
-    println!("{:?}", e);
-    false
-}
-
 
 fn main() -> Result<(), sust::err::Error> {
-    let a = sust::Vector(4, 2);
-    let b = sust::Vector(2.5, 3.25);
-
-    println!("{:?}", a.cast::<f32>() + b);
-
-    let window = sust::window::new("Sust window", sust::Vector(400, 400), Some(sust::Vector(400, 400)));
-    pollster::block_on(window.run(events))?;
+    let context = sdl2::init().unwrap();
+    
+    println!("h");
 
     Ok(())
 }
