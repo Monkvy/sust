e7775d67324fbdf9a471596cfdeb6347a5926a3f
Monkvy <moluecke@gmx.de>
2022-10-19 22:55:18 +0200

    Docs

--- src/grid.rs
+++ src/grid.rs
@@ -1,18 +1,22 @@
 use crate::{Config, Color, Vector, RenderWindow, Rect};
 
 
+/// Use this trait on a struct to uses it
+/// as cell inside a Grid.
+/// 
+/// ### Methods
+/// * `default()` -> Self - The initial state of every cell.
+/// * `color(&self)` -> Color - Change the cell color based on some conditions if you want.
+/// * `border(&self)` -> (u16, Color) - Change the cell border if you want.
 pub trait Cell {
     fn default() -> Self;
-
-    fn color(&self) -> Color {
-        Color::BLACK
-    }
-
-    fn border(&self) -> (u16, Color) {
-        (0, Color::TRANSPARENT)
-    }
+    fn color(&self) -> Color { Color::BLACK }
+    fn border(&self) -> (u16, Color) { (0, Color::TRANSPARENT) }
 }
 
+/// A grid stores a 2d array of cells that 
+/// are rendered to the window.
+/// Call Grid::new to create one.
 #[derive(Clone)]
 pub struct Grid<T: Cell + Clone> {
     rows: u16,
@@ -22,22 +26,35 @@ pub struct Grid<T: Cell + Clone> {
 }
 
 impl<T: Cell + Clone> Grid<T> {
+    /// Create a new grid that stores cells that are rendered to the window.
+    /// The grid gets filled with the result of the Cell::default() function.
     pub fn new(rows: u16, cols: u16, scale: u16) -> Grid<T> {
         Grid::<T> { rows, cols, scale, cells: vec![vec![T::default(); cols as usize]; rows as usize] }
     }
 
+    /// Get the window config based on the size & scale of the grid.
     pub fn win_conf(&self) -> Config {
         Config::grid(self.rows, self.cols, self.scale)
     }
 
+    /// Get the rows & cols of this grid.
     pub fn get_size(&self) -> Vector<u16> {
         Vector(self.rows, self.cols)
     }
 
+    /// Get the scale.
     pub fn get_scale(&self) -> u16 {
         self.scale
     }
 
+    /// Map the given vector onto the grid based on the scale
+    /// and get the corresponding cell.
+    /// 
+    /// ### Arguments
+    /// * `vec`: [Into]<[Vector<[u16]>]> - The position to map.
+    ///
+    /// ### Returns
+    /// A [Option]<&mut [Cell]> containing the corresponding cell.
     pub fn map_vec<V: Into<Vector<u16>>>(&mut self, vec: V) -> Option<&mut T> {
         let v: Vector<u16> = vec.try_into().unwrap();
         let pos = v / self.scale;
@@ -46,6 +63,7 @@ impl<T: Cell + Clone> Grid<T> {
         Some(&mut self.cells[pos.1 as usize][pos.0 as usize])
     }
 
+    /// Renders the grid to the given window.
     pub fn render(&self, window: &mut RenderWindow) {
         for (r, row) in self.cells.iter().enumerate() {
             for (c, cell) in row.iter().enumerate() {
