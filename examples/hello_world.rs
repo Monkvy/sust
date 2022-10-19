aeffcd47290f3ea46e6ef7d5c42fc16708168d4a
Monkvy <moluecke@gmx.de>
2022-10-19 23:27:48 +0200

    Update hello_world.rs

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -3,8 +3,7 @@ struct App;
 impl sust::App for App {
     fn events(&mut self, _event: sust::Event) -> bool { false }
     fn update(&mut self, _dt: f32) {}
-    fn render(&self, _window: &mut sust::RenderWindow) {
-}
+    fn render(&self, _window: &mut sust::RenderWindow) {}
 }
 
 fn main() {
